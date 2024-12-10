d9080be00fa671a2a803600bd24c8617731f4c9b
FoodBridge-cell <foodbridgeapp@gmail.com>
2024-12-10 13:04:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,15 +4,15 @@
 VOTING_URL = "https://mastersunion.org/events/hssl-leaderboard-details/67513adf30ca264e0eff504a"
 
 # Gmail credentials
-GMAIL_USERNAME = "foodbridgeapp@gmail.com"
-GMAIL_PASSWORD = "your-app-password"  # Use an app password for Gmail if 2FA is enabled
+GMAIL_USERNAME = "turbsturton@gmail.com"
+GMAIL_PASSWORD = ""  # Use an app password for Gmail if 2FA is enabled
 
 # Email pattern for voting
-EMAIL_PREFIX = "foodbridgeapp+"
+EMAIL_PREFIX = "turbsturton+"
 EMAIL_DOMAIN = "@gmail.com"
 
 # Default institution name
-INSTITUTION_NAME = "Delhi Public School"
+INSTITUTION_NAME = "Delhi Public School Nacharam"
 
 # Log file path
 LOG_FILE = "logs/voting_log.txt"
